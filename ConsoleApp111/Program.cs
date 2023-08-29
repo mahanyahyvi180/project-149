ff3740ed19a524b8e71ce35168a8709c8815543d
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-29 10:18:37 +0330

    Add files via upload

---
+++ ConsoleApp111/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int gradecounter = 0;
+            int aCount = 0;
+            int bCount = 0;
+            int cCount = 0;
+            int dCount = 0;
+            int fCount = 0;
+
+            Console.WriteLine("Enter the integer grades in the range 0-100.");
+            Console.WriteLine("Type<Ctrl>z and press Enter to terminate input:");
+
+            string input = Console.ReadLine();
+
+
+            while (input != null)
+            {
+                int grade = int.Parse(input);
+                total += grade;
+                ++gradecounter;
+
+                switch (grade / 10)
+                {
+                    case 9:
+                    case 10:
+                        ++aCount;
+                        break;
+                    case 8:
+                        ++bCount;
+                        break;
+                    case 7:
+                        ++cCount;
+                        break;
+                    case 6:
+                        ++dCount;
+                        break;
+                    default:
+                        ++fCount;
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nGrade Report:");
+
+            if (gradecounter != 0)
+            {
+
+                double average = (double)total / gradecounter;
+
+                Console.WriteLine($"Total of the {gradecounter} grades entered is{total}");
+                Console.WriteLine($"Class average is{average:F}");
+                Console.WriteLine("Number of students who received each grade:");
+                Console.WriteLine($"A: {aCount}");
+                Console.WriteLine($"B:{bCount}");
+                Console.WriteLine($"C:{cCount}");
+                Console.WriteLine($"D:{dCount}");
+                Console.WriteLine($"F:{fCount}");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("No Grades were entered");
+                Console.ReadLine();
+            }
+        }
+    }
+}
